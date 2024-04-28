290338a018f88500f55be9ff3c20e803351af56c
sadar786 <bathoorsadarullah@gmail.com>
2024-04-28 15:50:23 +0500

    connection to the mongodb is done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+
+
+try{
+const connected = mongoose.connect(process.env.MONGO)
+}
+catch(err){
+    console.log("error occur to connent to database"+ err)
+}
 app.listen(3000 , ()=>{
     console.log("app is listen on 3000")
 })
+
+
+
+
+// mongodb+srv://myblog-mern:<password>@myblog-mern.ueldain.mongodb.net/
