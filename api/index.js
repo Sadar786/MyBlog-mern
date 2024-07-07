6b32bab9d7f9bf41d464343c5b391031bcdfe79f
sadar786 <bathoorsadarullah@gmail.com>
2024-07-07 13:28:14 +0500

    add pre installations

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRouter from './routes/post.router.js';
 import commentRouter from './routes/comment.routes.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
 app.use(express.json())
@@ -26,12 +27,17 @@ app.listen(3000 , ()=>{
     console.log("app is listen on 3000")
 })
 
+const __dirname = path.resolve();
+
 app.use('/api/user', UserRouter);
 app.use('/api/auth', AuthRouter);
 app.use('/api/post', postRouter)
 app.use('/api/comment', commentRouter)
 
-
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use((error, req, res, next) =>{
     const statusCode = error.statusCode || 500;
     const message = error.message || "server internal error";
